6ee95fdd0c39c9bbc6997ecd8d60ff6bce18dc75
Jayasurya M <64420002+mj-surya@users.noreply.github.com>
2022-03-25 15:18:29 +0530

    Update Cart.js

--- Cart.js
+++ Cart.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import './style.css';
+import './ContextProvider';
+
+import { CartContextValue } from './ContextProvider';
 
 function Cart() {
- return ( 
-   <div class = "cartPage" >
-        <div class = "cartcontent center" >
-          <table >
-            <tr class = "cartDetails" >
-              <th class = "center-items" > Items < /th>  
-              <th class = "center-head" > Quantity < /th>  
-              <th class = "center-head" > Price < /th>  
-              <th class = "center-head" > Total < /th>  
-            </tr > 
-            <tr >
-              <td class = "left" > asdfghjk < /td>  
-              <td > 5 < /td>  
-              <td > 52 < /td>  
-              <td > 850 < /td>  
-            </tr >
-          </table>  
-        </div > 
-        <div class = "orderButton center" >
-          <button class = "orderbtn" href = "" > Place Order < /button>  
-        </div > 
-    </div>
+    const [cartData,dispatch] = CartContextValue();
+    return ( 
+        <div class = "cartPage">
+            <div class = "cartcontent center">
+            <table>
+            <tr class = "cartDetails">
+                <th class = "center-items" > Items </th>   
+                <th class = "center-head" > Quantity </th>   
+                <th class = "center-head" > Price </th>   
+                <th class = "center-head" > Total </th>   
+            </tr>
+
+            {
+                cartData.cartItems.map(cartObj => ( 
+                    <tr>
+                    <td class = "left" > { cartObj.item } </td>   
+                    <td >{ cartObj.qty } </td>   
+                    <td >{ cartObj.price } </td>   
+                    <td >{ cartObj.total } </td>   
+                    </tr>
+                ))
+            }
+
+
+            </table>   
+            </div>  
+            <div class = "orderButton center" >
+                <button class = "orderbtn" href = "" > Place Order </button>   
+            </div>  
+        </div>
     )
 }
 
